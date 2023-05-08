fdeb25425227a708d6a7dd9c075208acbcf127fc
ghjattu <icracked369@gmail.com>
2023-05-08 16:51:50 +0800

    cors

--- server/main.go
+++ server/main.go
@@ -5,10 +5,12 @@ import (
 	"server/routes"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	app := fiber.New()
+	app.Use(cors.New())
 	routes.RegisterRoutes(app)
 
 	log.Fatalln(app.Listen(":8080"))
